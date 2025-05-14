1303d2e9cb3292ae0be5da10a2fe3bb960b70591
Søren Vestergren Sandgaard <soren.sandgaard@sund.ku.dk>
2025-05-14 14:13:50 +0200

    Returned from samtools to 10x bamtofastq

--- entrypoint_method.py
+++ entrypoint_method.py
@@ -26,15 +26,28 @@ def run_method(output_dir, name, bam_input, ref_input, parameters):
     content += a.stdout
     content += f"\n\n"
 
-    # Run samtools fastq to generate fastq files
-    R1_out = f"{output_dir}/{name}.anon_R1.fastq"
-    R2_out = f"{output_dir}/{name}.anon_R2.fastq"
-    unpaired_out = f"{output_dir}/{name}.anon_unpaired.fastq"
-    bamtofastq_command = f"samtools fastq -1 {R1_out} -2 {R2_out} -s {unpaired_out} {anon_bam_pos}"
+    # Run bamtofastq
+    anon_fastq_pos = f"{output_dir}/anon_fastqs"
+    bamtofastq_command = f"bamtofastq --nthreads=16 {anon_bam_pos} {anon_fastq_pos}"
+    content = f"Bamtofastq command:\n{bamtofastq_command}\n"
     a = subprocess.run(bamtofastq_command.split(),capture_output=True,text=True)
-    content += f"Bamtofastq output:\n"
-    content += a.stdout
-    content += f"\n\n"
+    content += f"Bamtofastq output:\n{a.stdout}\n\n"
+
+    # Find name of bamtofastq folder
+    a = subprocess.run(f"ls {anon_fastq_pos}".split(),capture_output=True,text=True)
+    content += f"fastq foldername object: {a.stdout}\n"
+    fastq_foldername = a.stdout[:-1]
+    content += f"fastq foldername: {fastq_foldername}\n\n"
+
+    # Run samtools fastq to generate fastq files
+    # R1_out = f"{output_dir}/{name}.anon_R1.fastq"
+    # R2_out = f"{output_dir}/{name}.anon_R2.fastq"
+    # unpaired_out = f"{output_dir}/{name}.anon_unpaired.fastq"
+    # bamtofastq_command = f"samtools fastq -1 {R1_out} -2 {R2_out} -s {unpaired_out} {anon_bam_pos}"
+    # a = subprocess.run(bamtofastq_command.split(),capture_output=True,text=True)
+    # content += f"Bamtofastq output:\n"
+    # content += a.stdout
+    # content += f"\n\n"
 
     content += f"All clear - successfull run"
 
